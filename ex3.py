45b5ed04d56a1ddab773fa93c554f1f57e0f5123
Marshall <mpkrass@gmail.com>
2017-02-03 14:36:30 -0500

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,21 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
+
+for number in a:
+    if number < 5:
+        print number
+        b.append(number)
+print b
+
+def list_lower():
+    try:
+        given_number = int(raw_input("Please provide an integer with a maximum of two digits> "))
+        for number in a:
+            if number < int(given_number):
+                print number
+                b.append(number)
+        print b
+    except ValueError:
+        print "That is not an integer"
+        list_lower()
+list_lower()
