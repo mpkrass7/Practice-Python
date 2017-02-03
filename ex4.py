975cc4ad366e1dfaa263e11b40ee632543c3e902
Marshall <mpkrass@gmail.com>
2017-02-03 14:38:51 -0500

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,20 @@
+
+
+def int_checker():
+    while True:
+        try:
+            a = int(raw_input("Input an integer to find its divisors> "))
+        except ValueError:
+            print "That is not an integer"
+        else:
+            return a
+
+def divisors():
+    divisor = int_checker()
+    x = range(1, divisor+1)
+    y = []
+    for number in x:
+        if divisor % number == 0:
+            y.append(number)
+    print y
+divisors()
