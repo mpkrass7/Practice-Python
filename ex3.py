fa9c353d79dbade83a115070888571ad55371590
Marshall <mpkrass@gmail.com>
2017-02-03 14:38:08 -0500

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,3 +1,5 @@
+#Appends numbers from list a to list b if they are less than five
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = []
 
@@ -6,6 +8,8 @@ for number in a:
         print number
         b.append(number)
 print b
+
+#function that appends numbers from list A to list B if they are less than the input integer
 
 def list_lower():
     try:
